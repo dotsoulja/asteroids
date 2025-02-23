f2e4c0d868c6613da68d81e087f8a8c9e0b47221
dotsoulja <c3f1990@gmail.com>
2025-02-23 18:55:16 -0500

    added .gitignore with venv/ created main.py and requirements.txt

---
+++ main.py
@@ -0,0 +1,7 @@
+import pygame # Import the pygame library to allow us access to its code
+
+def main():
+    print("Starting Asteroids!")
+
+if __name__ == "__main__":
+    main() # Call the main when program starts

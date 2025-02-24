d9757220d3884a19c23d99bbd835ad453ac30612
dotsoulja <c3f1990@gmail.com>
2025-02-23 20:08:55 -0500

    updated fps in main, added circleshape file and class

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from constants import * # Import for access to constants
 
 def main():
     pygame.init() # Initialize the pygame library
+    clock = pygame.time.Clock() # Create a clock object to control the frame rate
+    dt = 0 # Set the delta time to 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Create the screen
     while True:
         screen.fill((0, 0, 0)) # Fill the screen with a color
+
         for event in pygame.event.get(): # Get all the events that have happened
             if event.type == pygame.QUIT: # If the event is a quit event, then quit the game
                 return # Exit the game
         pygame.display.flip() # Update the display
+        clock.tick(60) # Set the frame rate to 60 frames per second
+        dt = clock.tick(60) / 1000.0 # Set the delta time to the time since the last frame in seconds
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}") 
     print(f"Screen height: {SCREEN_HEIGHT}")
